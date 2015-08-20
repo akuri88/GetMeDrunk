2115f4be09fe49c563b07bd72861b1dc0553a084
lejandro83 <lejandro83@gmail.com>
2015-08-20 12:53:40 -0500

    Adds shoulda matchers for the spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,3 +11,8 @@ describe User do
 
   it { should be_valid }
 end
+
+it { should validate_presence_of(:email) }
+it { should validate_uniqueness_of(:email) }
+it { should validate_confirmation_of(:password) }
+it { should allow_value('example@domain.com').for(:email) }
